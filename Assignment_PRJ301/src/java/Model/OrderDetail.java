16721b25d12034aa781b8ec9eb4feb830d373daa
AkamexBii <trungtalon31@gmail.com>
2022-07-11 13:38:11 +0700

    Still upload in Model Package

--- Assignment_PRJ301/src/java/Model/OrderDetail.java
+++ Assignment_PRJ301/src/java/Model/OrderDetail.java
@@ -4,10 +4,59 @@
  */
 package Model;
 
+import java.text.DecimalFormat;
+
 /**
  *
  * @author ROG STRIX
  */
 public class OrderDetail {
+    private String dogID;
+    private String sex;
+    private int quantity;
+    private float total;
     
+    DecimalFormat df = new DecimalFormat("#.00");
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(String dogID, String sex, int quantity, float total) {
+        this.dogID = dogID;
+        this.sex = sex;
+        this.quantity = quantity;
+        this.total = Float.parseFloat(df.format(total));
+    }
+
+    public String getDogID() {
+        return dogID;
+    }
+
+    public void setDogID(String dogID) {
+        this.dogID = dogID;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = Float.parseFloat(df.format(total));
+    }
 }
